0d75d1cf4416ded6163ba5520675b90f08d26520
benjaminroche <benjamin.roche4@gmail.com>
2023-11-10 22:34:34 +0100

    feat : email

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -5,15 +5,20 @@ namespace App\Controller;
 use App\Entity\Contact;
 use App\Form\ContactType;
 use Doctrine\ORM\EntityManagerInterface;
+use Psr\Log\LoggerInterface;
+use Symfony\Bridge\Twig\Mime\TemplatedEmail;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Mailer\Exception\TransportExceptionInterface;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Email;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ContactController extends AbstractController
 {
     #[Route('/contact', name: 'app_contact')]
-    public function index(Request $request, EntityManagerInterface $entityManager): Response
+    public function index(Request $request, EntityManagerInterface $entityManager, MailerInterface $mailer, LoggerInterface $logger): Response
     {
         $contact = new Contact();
         $form = $this->createForm(ContactType::class, $contact);
@@ -23,6 +28,29 @@ class ContactController extends AbstractController
             $contact->setCreatedAt(new \DateTimeImmutable());
             $entityManager->persist($contact);
             $entityManager->flush();
+
+            $email = (new TemplatedEmail())
+                ->from($contact->getEmail())
+                ->to('contact@uniflow.agency')
+                ->subject('[Fiduciaire Genevoise] Nouvelle demande de contact')
+                ->htmlTemplate('emails/contact.html.twig')
+                ->context([
+                    'date' => new \DateTimeImmutable(),
+                    'fistName' => $contact->getFirstName(),
+                    'lastName' => $contact->getLastName(),
+                    'society' => $contact->getSociety(),
+                    'email' => $contact->getEmail(),
+                    'phone' => $contact->getPhone(),
+                    'message' => $contact->getMessage(),
+                    'help' => $contact->getHelp(),
+                ])
+            ;
+
+            try {
+                $mailer->send($email);
+            } catch (TransportExceptionInterface $e) {
+                $logger->error('Erreur lors de l\'envoi de l\'email :'. $e->getMessage());
+            }
 
             $this->addFlash('contactSuccess', 'Message envoyé. Un collaborateur va prendre contact avec vous rapidement.');
             return $this->redirectToRoute('app_contact');
