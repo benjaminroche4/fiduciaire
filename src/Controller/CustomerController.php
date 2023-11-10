e8931f553fc3dafbf7068e2f576974312a9e98cb
benjaminroche <benjamin.roche4@gmail.com>
2023-11-10 23:12:48 +0100

    feat : edit email

--- src/Controller/CustomerController.php
+++ src/Controller/CustomerController.php
@@ -7,15 +7,19 @@ use App\Entity\Customer;
 use App\Form\ContactType;
 use App\Form\CustomerType;
 use Doctrine\ORM\EntityManagerInterface;
+use Psr\Log\LoggerInterface;
+use Symfony\Bridge\Twig\Mime\TemplatedEmail;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Mailer\Exception\TransportExceptionInterface;
+use Symfony\Component\Mailer\MailerInterface;
 use Symfony\Component\Routing\Annotation\Route;
 
 class CustomerController extends AbstractController
 {
     #[Route('/devenir-client', name: 'app_devenir_client')]
-    public function devenirClient(Request $request, EntityManagerInterface $entityManager): Response
+    public function devenirClient(Request $request, EntityManagerInterface $entityManager, MailerInterface $mailer, LoggerInterface $logger): Response
     {
         $contact = new Customer();
         $form = $this->createForm(CustomerType::class, $contact);
@@ -25,6 +29,31 @@ class CustomerController extends AbstractController
             $contact->setCreatedAt(new \DateTimeImmutable());
             $entityManager->persist($contact);
             $entityManager->flush();
+
+            $email = (new TemplatedEmail())
+                ->from($contact->getEmail())
+                ->to('contact@uniflow.agency')
+                ->subject('[Fiduciaire Genevoise] Nouvelle demande pour devenir client')
+                ->htmlTemplate('emails/devenir-client.html.twig')
+                ->context([
+                    'createdAt' => new \DateTimeImmutable(),
+                    'fistName' => $contact->getFirstName(),
+                    'lastName' => $contact->getLastName(),
+                    'society' => $contact->getSociety(),
+                    'email' => $contact->getEmail(),
+                    'phone' => $contact->getPhone(),
+                    'position' => $contact->getPosition(),
+                    'numberEmployees' => $contact->getNumberEmployees(),
+                    'localitySociety' => $contact->getLocalitySociety(),
+                    'legalStatus' => $contact->getLegalStatus(),
+                ])
+            ;
+
+            try {
+                $mailer->send($email);
+            } catch (TransportExceptionInterface $e) {
+                $logger->error('Erreur lors de l\'envoi de l\'email :'. $e->getMessage());
+            }
 
             return $this->redirectToRoute('app_devenir_client_traitement');
         }
