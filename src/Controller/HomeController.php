7de97650289e694ca2a9fb593d91180660b70f9b
benjaminroche <benjamin.roche4@gmail.com>
2024-01-04 10:54:10 +0100

    fix: opti images

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,15 +2,26 @@
 
 namespace App\Controller;
 
+use App\Repository\PostsRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
 {
+    public function __construct(
+        private readonly PostsRepository $postsRepository
+    )
+    {
+    }
+
     #[Route('/', name: 'app_home')]
     public function home(): Response
     {
-        return $this->render('home/index.html.twig', []);
+        $posts = $this->postsRepository->findLastThree();
+
+        return $this->render('home/index.html.twig', [
+            'posts' => $posts,
+        ]);
     }
 }
