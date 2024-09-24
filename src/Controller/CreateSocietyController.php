4e8219b4f74f9d8f0cdbc6c69ca6272954a0a994
benjaminroche <benjamin.roche4@gmail.com>
2024-09-24 15:29:57 +0200

    feat:  maj

--- src/Controller/CreateSocietyController.php
+++ src/Controller/CreateSocietyController.php
@@ -8,9 +8,21 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class CreateSocietyController extends AbstractController
 {
-    #[Route('/creation-de-societe/SARL', name: 'app_create_society_SARL')]
+    #[Route('/creation-de-societe/societe-a-responsabilite-limitee', name: 'app_create_society_SARL')]
     public function sarl(): Response
     {
         return $this->render('create_society/sarl.html.twig');
+    }
+
+    #[Route('/creation-de-societe/societe-anonyme', name: 'app_create_society_SA')]
+    public function sa(): Response
+    {
+        return $this->render('create_society/sa.html.twig');
+    }
+
+    #[Route('/creation-de-societe/raison-individuelle', name: 'app_create_society_RI')]
+    public function ri(): Response
+    {
+        return $this->render('create_society/ri.html.twig');
     }
 }
