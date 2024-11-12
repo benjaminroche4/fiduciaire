47da118c2eacab548486c114d60fc23796cb9158
benjamin <benjamin.roche4@gmail.com>
2024-11-12 09:07:34 +0100

    edit seo

---
+++ src/Controller/OfficeController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class OfficeController extends AbstractController
+{
+    #[Route('/fiduciaire/geneve', name: 'app_office_geneva')]
+    public function officeGeneva(): Response
+    {
+        return $this->render('office/office_geneva.html.twig');
+    }
+
+    #[Route('/fiduciaire/lausanne', name: 'app_office_lausanne')]
+    public function officeLausanne(): Response
+    {
+        return $this->render('office/office_geneva.html.twig');
+    }
+
+    #[Route('/fiduciaire/bale', name: 'app_office_bale')]
+    public function officeBale(): Response
+    {
+        return $this->render('office/office_bale.html.twig');
+    }
+}
